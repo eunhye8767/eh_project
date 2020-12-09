c240feb480e15fa5f858dfe06155863f2df1f0ee
eunhye8767 <leh8767@gmail.com>
2020-12-09 15:34:00 +0900

    11. 종합예제) 전진! 3D 스크롤 - 5 캐릭터 생성자 함수 만들기
    - 강의 스크롤 16
    - 캐릭터 생성자 함수 만들기
      >> Character.js 참고
    
    - 클릭했을 때, .stage 자식요소로 추가할 예정.
      >> .house 와 형제 위치로 .house 뒤로 나오게 해주려면
         .stage 에서 appendChild로 적용해주면 된다.
    
    - 생성자 함수 호출하기.
      >> 생성자 함수를 호출할 때는 앞에 new 를 붙여주어야 한다. (= 인스턴스)
         new new Character();

---
+++ 11_ex_3Dscroll/js/Character.js
@@ -0,0 +1,56 @@
+// 캐릭터 생성자 함수 만들기
+/*
+   wall3d.js 파일에 생성자 함수를 호출할 때
+   생성자 이름 앞에 new 붙여서 호출해줘야 한다. (= 인스턴스)
+   >> new Character();
+ */ 
+function Character() {
+    /*
+        1. 캐릭터 클래스를 가졌던 제일 바깥에 div 엘리먼트를 생성
+           변수로 만드는 것이 아닌
+           this.mainElem =
+           >> this 로 하는 것은 캐릭터 생성자를 통해서 만들어 낼 
+              인스턴스 객체에 this.mainElem을 속성으로 쓰겠다는 것.
+           >> 변수가 아닌 하나의 속성으로 만들겠다는 것
+
+           >> body id="page" 일때, id (속성 attribute) / page (프로퍼티(속성 값) property)
+              this.mainElem 라는 속성 생성. this.mainElem = div 엘리먼트
+     */ 
+    this.mainElem = document.createElement('div');
+    this.mainElem.classList.add('character');
+
+    /*
+        innerHTML 로 이전에 만들었던 캐릭터 소스를 적용해준다.
+        '' 빈공란을 해주고
+        이전에 만들었던 캐릭터 마크업 그대로 + '' 을 통해 적용해준다.
+        >> es6 문법에서는 `` 하는 문법도 있다.
+     */ 
+    this.mainElem.innerHTML = ''
+        + '<div class="character-face-con character-head">'
+            + '<div class="character-face character-head-face face-front"></div>'
+            + '<div class="character-face character-head-face face-back"></div>'
+        + '</div>'
+        + '<div class="character-face-con character-torso">'
+            + '<div class="character-face character-torso-face face-front"></div>'
+            + '<div class="character-face character-torso-face face-back"></div>'
+        + '</div>'
+        + '<div class="character-face-con character-arm character-arm-right">'
+            + '<div class="character-face character-arm-face face-front"></div>'
+            + '<div class="character-face character-arm-face face-back"></div>'
+        + '</div>'
+        + '<div class="character-face-con character-arm character-arm-left">'
+            + '<div class="character-face character-arm-face face-front"></div>'
+            + '<div class="character-face character-arm-face face-back"></div>'
+        + '</div>'
+        + '<div class="character-face-con character-leg character-leg-right">'
+            + '<div class="character-face character-leg-face face-front"></div>'
+            + '<div class="character-face character-leg-face face-back"></div>'
+        + '</div>'
+        + '<div class="character-face-con character-leg character-leg-left">'
+            + '<div class="character-face character-leg-face face-front"></div>'
+            + '<div class="character-face character-leg-face face-back"></div>'
+        + '</div>';
+    
+    // .stage 의 자식요소에서 제일 끝에 추가하려면 (.house 와 형제)
+    document.querySelector('.stage').appendChild(this.mainElem);
+}
--- 11_ex_3Dscroll/js/wall3d.js
+++ 11_ex_3Dscroll/js/wall3d.js
@@ -109,4 +109,6 @@
 
     resizeHandler();
 
+    new Character();
+    
 })();
