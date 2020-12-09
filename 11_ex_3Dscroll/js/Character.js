51ac198930fecd9a62ec9a5e8db489019f2fd2ae
eunhye8767 <leh8767@gmail.com>
2020-12-09 16:51:01 +0900

    11. 종합예제) 전진! 3D 스크롤 - 7 스크롤 시작, 캐릭터 팔, 다리 애니메이션
    - 강의 스크롤 18~19
    - 클릭 후 생성된 캐릭터가 스크롤 시작됨과 동시에 팔, 다리 애니메이션 적용
      >> 스크롤 시 .character에 .running 클래스가 추가
    
    - Character.js 주석참고
      >> Character.prototype 관련
    
    - style.css
      /* 캐릭터에 running 클래스가 붙으면 팔다리가 움직임 (스크롤 이벤트) */

--- 11_ex_3Dscroll/js/Character.js
+++ 11_ex_3Dscroll/js/Character.js
@@ -18,6 +18,8 @@ function Character(info) {
      */ 
     this.mainElem = document.createElement('div');
     this.mainElem.classList.add('character');
+    // add로 다수의 클래스를 적용할 땐 ',' 로 구분해서 적용
+    // this.mainElem.classList.add('character','running');
 
     /*
         innerHTML 로 이전에 만들었던 캐릭터 소스를 적용해준다.
@@ -59,4 +61,40 @@ function Character(info) {
 
     // .character 의 left 값을 마우스 클릭한 위치로 바꿔주는 것
     this.mainElem.style.left = info.xPos + '%';
+
+    // 메서드 실행
+    this.init();
 }
+
+/*
+   >> 스크롤 시 캐릭터 자체에 효과를 주는 것이 때문에 캐릭터 생성자 함수에 적용을 한다.
+   >> 함수 생성자에서 공통으로 사용하는 속성이나 메서드는 'prototype' 객체로 만들어준다.
+
+      방법1. Character.prototype.xxxxx = function() {
+             }
+
+      방법2. Character.prototype = {
+                constructor : Character,
+                xxxxx = function() {
+                }
+             }; 
+
+ */ 
+Character.prototype = {
+    // Character 객체 속성을 재정의 하기 위해 constructor 설정을 해줌. 꼭 해야함! (기존 설정 보존)
+    constructor : Character,
+    init : function() {
+        /*
+            init 함수에 있는 this 와
+            window.addEventListener 의 this 는 다르기 떄문에
+            init 에서 가르키는 this를 사용하기 위해 상수를 만든다.
+            init 의 this 는 함수생성자가 만든 Character 를 가르키고
+            window.addEventListener 의 this 는 window 를 가르킴.
+         */ 
+        const self = this;
+        window.addEventListener('scroll', function() {
+            // this.mainElem.classList.add('running');
+            self.mainElem.classList.add('running');
+        });
+    }
+};
