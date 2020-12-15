539e9f80b7945a81547e42539514411861e35ef9
eunhye8767 <leh8767@gmail.com>
2020-12-15 17:10:19 +0900

    12. BBC 인터랙티브 페이지 클론 코딩 -2-3 js 기본 기능 완료
    - for문 = 기존 비효율적
      >> 필요없는 영역의 위치까지 루프를 계속 돈다.
      >> 현재 위치에 있는 필요한 영역만 체크할 수 있게 코드 개선
         ex) 말풍선 1,2 번의 영역 위치값만 확인 하면 되는데
         현재 코드에선 1~9번까지 위치값을 확인하기 위해 루프
    
    - IntersectionObserver
      >> 관찰하는 대상이 사라지거나 나타날 때
         그 시점마다 콜백함수가 실행이 된다
      >> console.log(entries) 로 정보들을 확인할 수 있다.
    
    - Intersection Observer 에는 다양한 기능이 있음.
    
    - Intersection Observer 요소를 이용하여
      현재 눈에 보이는 지 안 보이는 지 부분만을 체크
      >> IntersectionObserver ( 콜백함수가 들어가고 );
      >> IntersectionObserver ( (매개변수1, 매개변수2) => { } );
      >> IntersectionObserver( (entries, observer) => { } );
    
    1. io 를 이용해 IntersectionObserver 인스턴스 만들고
    
    2. observe 관찰이 되도록 등록
       >> io.observe(stepElems[i]);
       >> 현재 페이지에선 화면에 보여지는 인덱스 번호와
          위, 아래 인덱스 번호를 체크하면 된다.
       >> target 을 이용해 확인.
    
    3. ioIndex 변수를 만든다
       let ioIndex;
       >> 현재 체크되어 있는 값 = ioIndex
       >> 숫자가 문자열로 보여질 때 뒤에 *1 하면 숫자열로 바뀐다.
    
    4. for 문을 효율적으로 코드 개선
       for ( let i = ioIndex - 1; i < ioIndex + 2; i++ )

--- 12_bbc_clone/main.js
+++ 12_bbc_clone/main.js
@@ -7,8 +7,24 @@
     // 현재 활성화된(visible 클래스가 붙은) .graphic-item을 지정
     // 처음 브라우저 접속 시 첫번째 이미지가 보이게 설정
     let currentItem = graphicElems[0];
+    let ioIndex;
+
+    const io = new IntersectionObserver( (entries, observer) => {
+        // console.log(entries[0].target.dataset.index);
+        // 이제, observe 관찰이 되도록 등록을 해줘야 함. io.observe(stepElems[i])
+
+        // 현재 브라우저에서 보여지는 인덱스 번호 체크
+        ioIndex = entries[0].target.dataset.index * 1;
+        // ioIndex를 콘솔로 확인해보면 문자열로 나오는 것을 확인할 수 있다.
+        // 그래서 ioIndex를 숫자로 변환해준다. *1 해주면 숫자로 변환됨
+        // console.log(ioIndex);
+    });
 
     for ( let i = 0; i < stepElems.length; i++ ) {
+
+        // stepElems (.step) 이 전부 관촬 대상이 된다.
+        io.observe(stepElems[i]);
+
         // stepElems[i].setAttribute('data-index', i)
         stepElems[i].dataset.index = i;
         graphicElems[i].dataset.index = i;
@@ -28,8 +44,13 @@
         let step;
         let boundingRect;
 
-        for ( let i = 0; i < stepElems.length; i++ ) {
+        // for ( let i = 0; i < stepElems.length; i++ ) {
+        // ioIndex - 1   => 현재 index의 바로 이전
+        // ioIndex + 2   => 이전, 현재, 다음 
+        for ( let i = ioIndex - 1; i < ioIndex + 2; i++ ) {
             step = stepElems[i];
+            // 처음 step 값은 0 으로 에러 발생. 그래서 값이 없을 경우 for문 작동X
+            if ( !step ) continue;
             boundingRect = step.getBoundingClientRect();
 
             if ( boundingRect.top > window.innerHeight * 0.1 &&
