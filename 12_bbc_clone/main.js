2db91fbdc5b2f576d9d336e060956f3d9cb5eae5
eunhye8767 <leh8767@gmail.com>
2020-12-16 15:23:50 +0900

    12. BBC 인터랙티브 페이지 클론 코딩 -3-2 js 애니메이션(2)
    - 특정 위치가 되었을 때 새(bird) 애니메이션 주기
    
    - 활성화 함수에만 적용을 해주면
      1번만 실행되고, 그 다음부터는 실행된 마지막 지점에 계속 위치되어 있다.
      해당 지점이 올 때마다 반복 실행이 되게 코드 개선
    
      >> function inactivate() 에도 actions[action](false); 값을 주는데
           activate 에는 true, inactivate 에는 false 를 준다
      >> 메서드에 true, false 값을 받아오게 코드를 개선한다.
           birdFlies(key) {
              if ( key ) { } else { }
           }
    
      >> 넘겨지는 값 : argument(인수)
           넘겨오는 값을 저장할 그릇 : parameter(매개변수)
    
    - 2번째 새(bird) 애니메이션 적용 (birdFiles2)
    
    - will-change : 바뀔것을 미리 브라우저에 알려준다.
      >> 브라우저가 효율적으로 좀 더 빠르게 처리
      >> 그렇다고 너무 많이 사용하게 되면 브라우저가 과부하 = 속도저하
         적절히, 제한적으로 사용해야 한다.
      >> style.css
    
    - 새로고침하면 상단 위로 이동되게
      >> window.addEventListener('load' 이벤트 적용

--- 12_bbc_clone/main.js
+++ 12_bbc_clone/main.js
@@ -3,8 +3,24 @@
 
     // birdFlies 메서드로 등록
     const actions = {
-        birdFlies() {
-            document.querySelector('[data-index="2"] .bird').style.transform = `translateX(${window.innerWidth}px)`;
+        birdFlies(key) {
+            if (key) {
+                // 값이 있으면 
+                // 메서드를 불러올 때 actions[action](true); true 값을 주었기 때문에 key = true
+                document.querySelector('[data-index="2"] .bird').style.transform = `translateX(${window.innerWidth}px)`;
+            } else {
+                // 값이 없으면
+                // 메서드를 불러올 때 actions[action](false); false 값을 주었기 때문에 key = false
+                document.querySelector('[data-index="2"] .bird').style.transform = `translateX(-100px)`;
+            }
+        },
+        birdFlies2(key) {
+            if (key) {
+                // 위쪽으로 날라가게 
+                document.querySelector('[data-index="5"] .bird').style.transform = `translate(${window.innerWidth}px, ${-window.innerHeight * 0.7}px)`;
+            } else {
+                document.querySelector('[data-index="5"] .bird').style.transform = `translateX(-100px)`;
+            }
         }
     }
 
@@ -40,15 +56,22 @@
     // 활성화 - currentItem 
     function activate(action) {
         currentItem.classList.add('visible');
-        if ( action ) {
+        if (action) {
             // actions 메서드를 실행한다.
-            actions[action]();
+            // 활성화에만 적용하면 1번 실행, 실행된 마지막 지점에 적용되어 있다.
+            // 반복 실행을 위해 inactivate 에 초기화 값을 적용해준다.
+            // true 가 들어오면 1번이 실행 false 가 들어오면 2번이 실행되게
+            // 메서드에 true, false 자리를 만든다.
+            actions[action](true);
         }
     }
 
     // 비활성화 - currentItem 
-    function inactivate() {
+    function inactivate(action) {
         currentItem.classList.remove('visible');  
+        if (action) {
+            actions[action](false);
+        }
     }
 
     window.addEventListener('scroll', () => {
@@ -66,13 +89,24 @@
 
             if ( boundingRect.top > window.innerHeight * 0.1 &&
                  boundingRect.top < window.innerHeight * 0.8 ) {
-                    inactivate();
+                    
+                    // 반복 작업을 위해 인수값 적용
+                    inactivate(currentItem.dataset.action);
                     currentItem = graphicElems[step.dataset.index];
                     
                     // 현재 활성화된 currentItem 의 data-action 값을 가져와야 해서 아래처럼 써준다.;
                     activate(currentItem.dataset.action);
             }
         }
+    });
+
+    // 새로고침하면 상단 위로 이동되게
+    window.addEventListener('load', () => {
+
+        setTimeout( () =>  scrollTo(0, 0), 100 );
+        // scrollTo(x좌표, y좌표);
+        // scrollTo 는 타이밍상 조금 늦처줘야 잘 작동된다. 그래서 setTimeout 사용
+        // scrollTo(0, 0);
     });
 
     activate();
