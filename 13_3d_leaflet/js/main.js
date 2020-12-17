68fd495995585d7a185d26688f8ea74ec0b31437
eunhye8767 <leh8767@gmail.com>
2020-12-17 16:50:19 +0900

    13. 3D 리플릿 - 2 카드 css 디자인 적용
    - 강의 #5
    - 카드 마크업/css 적용
    - 카드가 모두 오픈되면 close 버튼 보이게
      >> pageCount 변수를 만들어서
         특정 숫자가 되면 body에 클래스 추가 > close 버튼 보이게 적용

--- 13_3d_leaflet/js/main.js
+++ 13_3d_leaflet/js/main.js
@@ -1,6 +1,9 @@
 ( () => {
 	const leaflet = document.querySelector('.leaflet');
 
+	// 페이지가 2번 클릭되어 열렸을 때 close 버튼이 보이게
+	let pageCount = 0;
+
 	// e.target 에 클래스 명이 맞는 지 함수로 만들었을 때
 	// 피라미트값에 e.target => elem, 'page' => className
 	function getTarget(elem, className) {
@@ -21,6 +24,10 @@
 		
 		if ( pageElem ) {
 			pageElem.classList.add('page-flipped');
+			pageCount++;
+			if ( pageCount == 2) {
+				document.body.classList.add('leaflet-opened');
+			}
 		}
 
 	})
